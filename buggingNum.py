e1cb4237c87685d0da8cf6a3fa5a071eb33cf5cf
sipoco <richard.auditore@gmail.com>
2017-05-18 16:10:38 +0200

    add new file re bugging

---
+++ buggingNum.py
@@ -0,0 +1,8 @@
+print('Enter the first number to add:')
+first = input()
+print('Enter the second number to add:')
+second = input()
+print('Enter the third number to add:')
+third = input()
+print('The sum is ' + first + second + third)
+''' see the bugNum.py for resolution, as this program results in the wrong result (concatenate instead of sum).
